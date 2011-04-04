f33598b94f5e2e03501ab1acd8e9729db63a16b1
Arman Vartan <Arman Vartan@localhost>
2011-04-04 17:35:06 +0200

    Added CSV and XML converter

--- modules/tools/diagnosisreader/src/net/xeoh/plugins/diagnosisreader/DiagnosisReader.java
+++ modules/tools/diagnosisreader/src/net/xeoh/plugins/diagnosisreader/DiagnosisReader.java
@@ -1,19 +1,19 @@
 /*
  * DiagnosisReader.java
- * 
+ *
  * Copyright (c) 2011, Ralf Biedert All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without modification, are
  * permitted provided that the following conditions are met:
- * 
+ *
  * Redistributions of source code must retain the above copyright notice, this list of
  * conditions and the following disclaimer. Redistributions in binary form must reproduce the
  * above copyright notice, this list of conditions and the following disclaimer in the
  * documentation and/or other materials provided with the distribution.
- * 
+ *
  * Neither the name of the author nor the names of its contributors may be used to endorse or
  * promote products derived from this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
  * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
@@ -23,7 +23,7 @@
  * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
  * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- * 
+ *
  */
 package net.xeoh.plugins.diagnosisreader;
 
@@ -37,7 +37,9 @@ import net.xeoh.plugins.base.impl.PluginManagerFactory;
 import net.xeoh.plugins.base.util.PluginManagerUtil;
 import net.xeoh.plugins.base.util.uri.ClassURI;
 import net.xeoh.plugins.diagnosisreader.converters.Converter;
+import net.xeoh.plugins.diagnosisreader.converters.impl.csv.CSVConverterImpl;
 import net.xeoh.plugins.diagnosisreader.converters.impl.plain.PlainConverterImpl;
+import net.xeoh.plugins.diagnosisreader.converters.impl.xml.XMLConverterImpl;
 import net.xeoh.plugins.diagnosisreader.ui.MainWindow;
 
 /**
@@ -49,18 +51,21 @@ public class DiagnosisReader {
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         } catch (final Exception e) {}
-        
+
         // We only need the default plugins and a few of our owns
         final PluginManager pluginManager = PluginManagerFactory.createPluginManager();
         final PluginManagerUtil pluginManagerUtil = new PluginManagerUtil(pluginManager);
-        
+
         pluginManager.addPluginsFrom(ClassURI.PLUGIN(PlainConverterImpl.class));
+        pluginManager.addPluginsFrom(ClassURI.PLUGIN(CSVConverterImpl.class));
+        pluginManager.addPluginsFrom(ClassURI.PLUGIN(XMLConverterImpl.class));
         pluginManager.addPluginsFrom(new File("plugins/").toURI());
-        
+
         final MainWindow mainWindow = new MainWindow(pluginManager);
         mainWindow.setVisible(true);
-        
+
         final Collection<Converter> converters = pluginManagerUtil.getPlugins(Converter.class);
+
         for (Converter converter : converters) {
             mainWindow.registerHandler(converter);
         }
---
+++ modules/tools/diagnosisreader/src/net/xeoh/plugins/diagnosisreader/converters/impl/xml/XMLConverterImpl.java
@@ -0,0 +1,111 @@
+/*
+ * ConverterImpl.java
+ *
+ * Copyright (c) 2011, Ralf Biedert All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list of
+ * conditions and the following disclaimer. Redistributions in binary form must reproduce the
+ * above copyright notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ *
+ * Neither the name of the author nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+package net.xeoh.plugins.diagnosisreader.converters.impl.xml;
+
+import static net.jcores.CoreKeeper.$;
+
+import java.io.File;
+import java.io.Serializable;
+
+import net.jcores.interfaces.functions.F1;
+import net.xeoh.plugins.base.annotations.PluginImplementation;
+import net.xeoh.plugins.base.annotations.injections.InjectPlugin;
+import net.xeoh.plugins.diagnosis.local.Diagnosis;
+import net.xeoh.plugins.diagnosis.local.DiagnosisMonitor;
+import net.xeoh.plugins.diagnosis.local.DiagnosisStatus;
+import net.xeoh.plugins.diagnosis.local.options.status.OptionInfo;
+import net.xeoh.plugins.diagnosisreader.converters.Converter;
+import net.xeoh.plugins.diagnosisreader.converters.ConverterInfo;
+
+/**
+ * @author Ralf Biedert
+ */
+@PluginImplementation
+public class XMLConverterImpl implements Converter {
+    /** */
+    @InjectPlugin
+    public Diagnosis diagnosis;
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see net.xeoh.plugins.diagnosisreader.converters.Converter#getInfo()
+     */
+    @Override
+    public ConverterInfo getInfo() {
+        return new ConverterInfo() {
+
+            @Override
+            public String getName() {
+                return "XML Converter";
+            }
+        };
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see net.xeoh.plugins.diagnosisreader.converters.Converter#convert(java.io.File)
+     */
+    @Override
+    public void convert(File file) {
+        final String xmlHeader = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+        final String startTag = "<LogEntry>\n".intern();
+        final String endTag = "</LogEntry>\n".intern();
+
+        final StringBuilder sb = new StringBuilder(100000);
+        sb.append(xmlHeader);
+
+        this.diagnosis.replay(file.getAbsolutePath(), new DiagnosisMonitor<Serializable>() {
+
+            @Override
+            public void onStatusChange(DiagnosisStatus<Serializable> status) {
+                final long date = status.getDate();
+                final String sender = $(status.getChannelAsString()).split("\\.").get(-1);
+                final String value = status.getValue().toString();
+                final String opts = $(status.getInfos()).map(new F1<OptionInfo, String>() {
+                    @Override
+                    public String f(OptionInfo arg0) {
+                        return arg0.getKey() + ":" + $(arg0.getValue()).get("null");
+                    }
+                }).string().join(",");
+
+
+                sb.append(startTag);
+                sb.append("\t<time>" + date + "</time>\n");
+                sb.append("\t<sender>" + sender + "</sender>\n");
+                sb.append("\t<value>" + value + "</value>\n");
+                sb.append("\t<opts>" + opts + "</opts>\n");
+                sb.append(endTag);
+            }
+        });
+
+        // Write text to file
+        $(file.getAbsolutePath() + ".xml").file().delete().append(sb);
+    }
+}
